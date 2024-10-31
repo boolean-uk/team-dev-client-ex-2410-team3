38ef2827bb1ee95b046c872c66fe331aa5102d5a
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-31 13:59:22 +0100

    Refactor UserProfile component: Update profile data handling and form submission
    - Replace mock data with an API call to profiles/${profileId}.
    - Populate ProfilePage Component with Fetched Data:
      - Update the state in ProfilePage.jsx to store data fetched from the API.
      - Ensure all fields are populated dynamically with the retrieved data.
    - Add validation for required fields before form submission.
    - Display an alert if any required fields are missing.

--- src/pages/profilePage/index.js
+++ src/pages/profilePage/index.js
@@ -1,6 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { user } from '../../service/mockData';
 import Card from '../../components/card';
 import './profilePage.css';
 import ProfileHeader from './components/ProfileHeader';
@@ -25,21 +24,31 @@ const UserProfile = ({ isEditMode }) => {
   const [isCurrentUserProfile, setIsCurrentUserProfile] = useState(false);
   const [isCurrentUserTeacher, setIsCurrentUserTeacher] = useState(false);
 
-  const { token, role } = useAuth();
+  const { token, role, onGetUser, onUpdateProfile } = useAuth();
   const { userId } = jwt_decode(token);
+  console.log('role', role);
 
-  const fetchProfile = () => {
-    setProfile(user.user);
-    setRoolbackProfile(user.user);
-    setInitials(user.user.firstName[0] + user.user.lastName[0]);
-    /*     try {
-      const data = await get(`profiles/${profileId}`);
-      setProfile(data);
-      const initials = data.user.firstName[0] + data.user.lastName[0];
-      setInitials(initials);
+  const fetchProfile = async () => {
+    try {
+      const data = await onGetUser(profileId);
+
+      const profileWithDefaults = {
+        ...data,
+        role: data.role || 'Student',
+        specialism: data.specialism || 'Software Developer',
+        cohort_id: data.cohort_id || 'Cohort 4',
+        startDate: data.startDate || 'January 2023',
+        endDate: data.endDate || 'June 2023',
+        username: data.username || '',
+        mobile: data.mobile || '123456789'
+      };
+
+      setProfile(profileWithDefaults);
+      setRoolbackProfile(profileWithDefaults);
+      setInitials(profileWithDefaults.firstName[0] + profileWithDefaults.lastName[0]);
     } catch (error) {
       console.error('Error fetching profile data:', error);
-    } */
+    }
   };
 
   useEffect(() => {
@@ -52,11 +61,69 @@ const UserProfile = ({ isEditMode }) => {
     }
   }, [profileId]);
 
-  const handleSubmit = (e) => {
-    // Handle form submission
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate(`/profile/${userId}`);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    if (!validateRequiredFields(profile)) {
+      return;
+    }
+
+    console.log('bio', profile.biography);
+
+    const {
+      firstName,
+      lastName,
+      biography: bio,
+      username,
+      githubUsername,
+      profilePicture,
+      mobile
+    } = profile;
+
+    try {
+      await onUpdateProfile(
+        profileId,
+        firstName,
+        lastName,
+        bio,
+        username,
+        githubUsername,
+        profilePicture,
+        mobile
+      );
+
+      setRoolbackProfile(profile);
+      setInitials(profile.firstName[0] + profile.lastName[0]);
+      navigate(`/profile/${profileId}`);
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } catch (error) {
+      console.error('Error updating profile:', error);
+    }
+  };
+
+  const validateRequiredFields = (profile) => {
+    const requiredFields = [
+      { name: 'First Name', value: profile.firstName },
+      { name: 'Last Name', value: profile.lastName },
+      { name: 'Username', value: profile.username },
+      { name: 'Github Username', value: profile.githubUsername },
+      { name: 'Role', value: profile.role },
+      { name: 'Specialism', value: profile.specialism },
+      { name: 'Cohort', value: profile.cohort_id },
+      { name: 'Start Date', value: profile.startDate },
+      { name: 'End Date', value: profile.endDate },
+      { name: 'Email', value: profile.email },
+      { name: 'Mobile', value: profile.mobile }
+    ];
+
+    const missingFields = requiredFields.filter((field) => !field.value);
+
+    if (missingFields.length > 0) {
+      alert('Please fill in all the required fields');
+      return false;
+    }
+
+    return true;
   };
 
   const handleInputChange = (e) => {
